cb53d059c69882950f3dd69d0b89942b14ac2761
Nikolay Kuznetsov <Nikolay.Kuznetsov@jetbrains.com>
2021-11-23 23:29:31 +0100

    Convert CanvasFactory into an interface

--- projector-client-web/src/main/kotlin/org/jetbrains/projector/client/web/window/Window.kt
+++ projector-client-web/src/main/kotlin/org/jetbrains/projector/client/web/window/Window.kt
@@ -28,6 +28,7 @@ import kotlinx.dom.addClass
 import org.jetbrains.projector.client.common.DrawEvent
 import org.jetbrains.projector.client.common.SingleRenderingSurfaceProcessor
 import org.jetbrains.projector.client.common.canvas.DomCanvas
+import org.jetbrains.projector.client.common.canvas.DomCanvasFactory
 import org.jetbrains.projector.client.common.canvas.buffering.DoubleBufferedRenderingSurface
 import org.jetbrains.projector.client.common.canvas.buffering.UnbufferedRenderingSurface
 import org.jetbrains.projector.client.common.misc.ImageCacher
@@ -288,7 +289,7 @@ class Window(windowData: WindowData, private val stateMachine: ClientStateMachin
 
   companion object {
     fun createRenderingSurface(canvas: HTMLCanvasElement) = when (ParamsProvider.DOUBLE_BUFFERING) {
-      true -> DoubleBufferedRenderingSurface(DomCanvas(canvas))
+      true -> DoubleBufferedRenderingSurface(DomCanvasFactory, DomCanvas(canvas))
       false -> UnbufferedRenderingSurface(DomCanvas(canvas))
     }
   }
